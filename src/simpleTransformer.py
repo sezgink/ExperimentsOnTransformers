d64dc9ba2578e87c07c0ec961845e41ed564cf61
sezgink <sezginkutlu00@gmail.com>
2024-11-25 04:20:01 +0300

    Positional encoding for processing token positions

--- src/simpleTransformer.py
+++ src/simpleTransformer.py
@@ -62,6 +62,32 @@ class MultiHeadAttention(nn.Module):
         output = self.concat_tensor(output)
         output = self.W_o(output)
         return output
+    
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_length=3000) -> None:
+        super().__init__()
+
+        position = torch.arange(0,max_length).unsqueeze(1)
+        div_term = torch.arange(0,d_model,2) * (-torch.log(torch.tensor(10000.0)) / d_model)
+
+        pe = torch.zeros(max_length,d_model)
+        pe[:,0::2] = torch.sin(position * div_term)
+        pe[:,1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)  # Shape: (1, max_len, d_model)
+        self.register_buffer('pe', pe)  # Register as buffer to avoid tracking gradients
+        
+        
+
+    def forward(self,x):
+        # x: (batch_size, seq_len, d_model)
+        x = x + self.pe[:, :x.size(1)].to(x.device)
+        return x
+
+        
+        
+
+
 
 
 
